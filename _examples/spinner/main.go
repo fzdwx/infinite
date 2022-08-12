c6337a6da959b6cdb4937542599cb5442a3d1fb8
fzdwx <likelovec@gmail.com>
2022-08-12 19:46:35 +0800

    fix: On Windows from console read chinese failed

--- _examples/spinner/main.go
+++ _examples/spinner/main.go
@@ -1,26 +1,43 @@
 package main
 
 import (
-	inf "github.com/fzdwx/infinite"
-	"github.com/fzdwx/infinite/components"
-	"github.com/fzdwx/infinite/components/spinner"
-	"time"
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"log"
+	"os"
+	"unicode/utf16"
 )
 
 func main() {
-	_ = inf.NewSpinner(
-		spinner.WithShape(components.Dot),
-		//spinner.WithDisableOutputResult(),
-	).Display(func(spinner *spinner.Spinner) {
-		for i := 0; i < 10; i++ {
-			time.Sleep(time.Millisecond * 100)
-			spinner.Refreshf("hello world %d", i)
-		}
+	// Write a file containing UTF-16 encoded data.
+	const file = "./foo.data"
+	x := utf16.Encode([]rune("世界"))
+	of, err := os.OpenFile(file, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := binary.Write(of, binary.LittleEndian, x); err != nil {
+		log.Fatal(err)
+	}
+	of.Close()
 
-		spinner.Finish("finish")
+	// Read back the bytes from that file.
+	content, err2 := os.ReadFile(file)
+	if err2 != nil {
+		log.Fatal(err2)
+	}
 
-		spinner.Refresh("is finish?")
-	})
+	// Convert to string (assumes UTF-8).
+	fmt.Printf("%s\n", string(content))
 
-	time.Sleep(time.Millisecond * 100 * 15)
+	// Better conversion.
+	var nx [2]uint16
+	b := bytes.NewBuffer(content)
+	if err := binary.Read(b, binary.LittleEndian, &nx); err != nil {
+		log.Fatalf("binary.Read: %v", err)
+	}
+	fmt.Printf("after binary.Read: %+v\n", nx)
+	v := utf16.Decode(nx[:])
+	fmt.Printf("%c %c\n", v[0], v[1])
 }
--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.18
 
 require (
 	github.com/charmbracelet/bubbles v0.13.0
-	github.com/charmbracelet/bubbletea v0.22.0
+	github.com/charmbracelet/bubbletea v0.22.2
 	github.com/charmbracelet/lipgloss v0.5.0
 	github.com/duke-git/lancet/v2 v2.1.4
 	github.com/lucasb-eyer/go-colorful v1.2.0
@@ -16,13 +16,18 @@ require (
 
 require (
 	github.com/atotto/clipboard v0.1.4 // indirect
-	github.com/charmbracelet/harmonica v0.2.0 // indirect
 	github.com/containerd/console v1.0.3 // indirect
+	github.com/gogs/chardet v0.0.0-20211120154057-b7413eaefb8f // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/muesli/ansi v0.0.0-20211031195517-c9f0611b6c70 // indirect
 	github.com/muesli/cancelreader v0.2.2 // indirect
 	github.com/muesli/termenv v0.12.0 // indirect
 	github.com/rivo/uniseg v0.3.4 // indirect
-	golang.org/x/sys v0.0.0-20220808155132-1c4a2a72c664 // indirect
+	github.com/yuin/charsetutil v1.0.0 // indirect
+	golang.org/x/net v0.0.0-20220811182439-13a9a731de15 // indirect
+	golang.org/x/sys v0.0.0-20220811171246-fbc7d0a398ab // indirect
 	golang.org/x/term v0.0.0-20220722155259-a9ba230a4035 // indirect
+	golang.org/x/text v0.3.7 // indirect
 )
+
+replace github.com/charmbracelet/bubbletea => github.com/fzdwx/bubbletea v0.22.2
