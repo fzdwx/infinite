885d96fe883d2bad8ff1205754c87c9a5cb3fd3d
fzdwx <likelovec@gmail.com>
2022-08-17 11:20:10 +0800

    test

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,5 +2,6 @@ import {defineConfig} from 'vitepress'
 
 export default defineConfig({
     title: "infinite",
-    description: '测试测试测试测试'
+    description: '测试测试测试测试',
+    base: "/infinite/",
 })
