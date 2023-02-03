5add8725c198ab1851d0a7840a08ee6f59b11b34
fzdwx <likelovec@gmail.com>
2023-02-03 20:05:46 +0800

    revent

--- components/selection/singleselect/single_select.go
+++ components/selection/singleselect/single_select.go
@@ -2,10 +2,9 @@ package singleselect
 
 import (
 	"fmt"
-	"os"
-
 	"github.com/fzdwx/infinite/components"
 	"github.com/fzdwx/infinite/components/selection/multiselect"
+	"github.com/rotisserie/eris"
 )
 
 type Select struct {
@@ -30,8 +29,8 @@ func (s *Select) Display(prompt ...string) (int, error) {
 		return -1, err
 	}
 
-	if len(hints) == 0 {
-		os.Exit(0)
+	if len(hints) <= 0 {
+		return -1, eris.New("not found selected")
 	}
 
 	return hints[0], nil
