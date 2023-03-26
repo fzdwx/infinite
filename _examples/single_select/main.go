4e4288b119b091f542559d702884cec8caf2d7b6
fzdwx <likelovec@gmail.com>
2023-03-26 20:57:31 +0800

    update example

--- _examples/single_select/main.go
+++ _examples/single_select/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/charmbracelet/bubbles/key"
 	inf "github.com/fzdwx/infinite"
 	"github.com/fzdwx/infinite/components/selection/singleselect"
 )
@@ -16,9 +18,18 @@ func main() {
 		"6 Buy car",
 		"7 Buy subway",
 	}
+
+	selectKeymap := singleselect.DefaultSingleKeyMap()
+	selectKeymap.Confirm = key.NewBinding(
+		key.WithKeys("enter"),
+	)
+	selectKeymap.Choice = key.NewBinding(
+		key.WithKeys("enter"),
+	)
 	selected, err := inf.NewSingleSelect(
 		options,
 		singleselect.WithDisableFilter(),
+		singleselect.WithKeyBinding(selectKeymap),
 	).Display("Hello world")
 
 	if err == nil {
