3273204a9e2eb37f505ead3d663c92495bedaa3a
fzdwx <likelovec@gmail.com>
2023-05-21 18:21:00 +0800

    chore: add dynamic single select

---
+++ _examples/dyn_single_select/main.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/charmbracelet/bubbles/key"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/fzdwx/infinite/components"
+	"github.com/fzdwx/infinite/components/selection/singleselect"
+	"github.com/fzdwx/iter"
+	"github.com/fzdwx/iter/stream"
+	"github.com/google/go-github/v52/github"
+)
+
+func main() {
+	m := &model{}
+	_, err := tea.NewProgram(m).Run()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(m.Res())
+}
+
+type model struct {
+	selection   *components.Selection
+	filterInput *components.Input
+	items       []string
+	client      *github.Client
+}
+
+func (m *model) Init() tea.Cmd {
+	m.client = github.NewClient(nil)
+	m.filterInput = components.NewInput()
+	m.selection = components.NewSelection(m.items)
+	m.selection.RowRender = func(cursorSymbol string, hintSymbol string, choice string) string {
+		return fmt.Sprintf("%s %s", cursorSymbol, choice)
+	}
+	keyMap := singleselect.DefaultSingleKeyMap()
+	m.selection.Keymap = components.SelectionKeyMap{
+		Up:     keyMap.Up,
+		Down:   keyMap.Down,
+		Choice: keyMap.Choice,
+		Confirm: key.NewBinding(
+			key.WithKeys("enter"),
+			key.WithHelp("enter", "search"),
+		),
+		Quit:     keyMap.Quit,
+		NextPage: keyMap.NextPage,
+		PrevPage: keyMap.PrevPage,
+	}
+	m.selection.FilterInput = m.filterInput
+
+	return m.selection.Init()
+}
+
+func (m *model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "enter":
+			m.refreshItem()
+			m.selection.Choices = mapper(m.items)
+			m.selection.RefreshChoices()
+			return m, nil
+		case "q", "esc":
+			return m, tea.Quit
+		}
+	}
+
+	var cmd tea.Cmd
+	_, cmd = m.selection.Update(msg)
+	return m, cmd
+}
+
+func mapper(items []string) []components.SelectionItem {
+	idx := 0
+	return stream.Map[string, components.SelectionItem](iter.Stream(items), func(item string) components.SelectionItem {
+		selectionItem := components.SelectionItem{
+			Val: item,
+			Idx: idx,
+		}
+		idx++
+		return selectionItem
+	}).ToArray()
+}
+
+func (m *model) View() string {
+	return m.selection.View()
+}
+
+func (m *model) refreshItem() {
+	repositories, _, err := m.client.Search.Repositories(context.Background(), m.filterInput.Value(), nil)
+	if err != nil {
+		return
+	}
+	m.items = iter.Stream(repositories.Repositories).MapTo(func(repository *github.Repository) string {
+		return repository.GetFullName()
+	}).ToArray()
+}
+
+func (m *model) Res() string {
+	value := m.selection.Value()
+	if len(value) == 0 {
+		return ""
+	}
+	return m.items[value[0]]
+}
--- go.mod
+++ go.mod
@@ -16,18 +16,28 @@ require (
 )
 
 require (
+	github.com/ProtonMail/go-crypto v0.0.0-20230217124315-7d5c6f04bbb8 // indirect
 	github.com/atotto/clipboard v0.1.4 // indirect
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/cloudflare/circl v1.1.0 // indirect
 	github.com/containerd/console v1.0.3 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-github/v52 v52.0.0 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/mattn/go-isatty v0.0.18 // indirect
 	github.com/mattn/go-localereader v0.0.1 // indirect
 	github.com/muesli/ansi v0.0.0-20230316100256-276c6243b2f6 // indirect
 	github.com/muesli/cancelreader v0.2.2 // indirect
 	github.com/muesli/termenv v0.15.1 // indirect
 	github.com/rivo/uniseg v0.4.4 // indirect
+	golang.org/x/crypto v0.7.0 // indirect
 	golang.org/x/exp v0.0.0-20230425010034-47ecfdc1ba53 // indirect
+	golang.org/x/net v0.9.0 // indirect
+	golang.org/x/oauth2 v0.7.0 // indirect
 	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/sys v0.7.0 // indirect
 	golang.org/x/term v0.7.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
 )
